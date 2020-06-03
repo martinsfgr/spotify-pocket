567415439319f2feaa769d2926d7f68480897243
Mauricio Fagner <52453558+martinsfgr@users.noreply.github.com>
2020-06-03 12:26:57 -0300

    Adicionar arquivos iniciais

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './containers';
+
+import './styles/globalStyles.scss'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
