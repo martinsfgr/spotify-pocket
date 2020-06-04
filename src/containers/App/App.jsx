af2443d4436c50112e718cf38701dd2ea22e8690
Mauricio Fagner <52453558+martinsfgr@users.noreply.github.com>
2020-06-03 22:28:57 -0300

    Adicionar provider para servir o estado global da aplicação

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Routes from '../../routes';
+
+import store from '../../redux/store'
 
 import './App.scss';
 
 const App = () => (
   <div className="app">
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   </div>
 );
 
