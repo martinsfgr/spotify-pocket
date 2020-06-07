066678c5993a3620540fd77189ebc31a6ba0232d
Mauricio Fagner <52453558+martinsfgr@users.noreply.github.com>
2020-06-07 16:36:06 -0300

    Criar container da dashboard

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { Player } from '../';
 
 import './Dashboard.scss';
 
-const Dashboard = ({ children }) => (<div className="dashboard" />);
+const Dashboard = ({ children }) => (
+  <div 
+    className="dashboard"
+    data-testid="dashboard"
+    style={{paddingBottom: `${playerHeight}px`}}
+  >
+    {children}
+
+    <Player />
+  </div>
+);
+
+Dashboard.defaultProps = {
+  children: [],
+}
+
+Dashboard.PropTypes = {
+  children: PropTypes.node,
+}
 
 export default Dashboard;
-
