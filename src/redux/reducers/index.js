e1d9358f9bff4cb5a244a403742ac8b2e8bead7c
Mauricio Fagner <52453558+martinsfgr@users.noreply.github.com>
2020-06-06 21:57:52 -0300

    Agrupar o reducer do conteúdo no arquivo principal

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import authReducer from './auth';
+import contentReducer from './content';
 
 const reducers = combineReducers({
   auth: authReducer,
+  content: contentReducer,
 })
 
 export default reducers;
