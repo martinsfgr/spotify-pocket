cce20a7e5395efbefb1b92184b0dbe6f8bbfa101
Mauricio Fagner <52453558+martinsfgr@users.noreply.github.com>
2020-06-07 22:12:28 -0300

    Criar container dos cards únicos das playlists

--- src/containers/Playlists/PlaylistItem.jsx
+++ src/containers/Playlists/PlaylistItem.jsx
@@ -1,6 +1,33 @@
 import React  from 'react';
+import { Link } from 'react-router-dom';
+import Ink from 'react-ink';
+import PropTypes from 'prop-types';
 
-const PlaylistItem = ({ categoryId, description, id, image, name, path }) => (<div className="playlists__item" />)
+const PlaylistItem = ({ categoryId, description, id, image, name, path }) => (
+  <div className="playlists_item" data-testid="playlists_item">
+    <Link
+      className="playlists__item__link"
+      style={{backgroundImage: `url(${image.url})`}}
+      title={name}
+      to={`${path}/${categoryId}/${id}`}
+    >
+      <Ink />
+    </Link>
+
+    <p className="playlists__item__description">
+      <strong>{name}</strong>
+
+      {description}
+    </p>
+  </div>
+)
+
+PlaylistItem.propTypes = {
+  categoryId: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+}
 
 export default PlaylistItem;
-
